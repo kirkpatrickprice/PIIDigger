a04055984e13a74d916717665893677f07ed5f93
Randy Bartels <rjbartels@outlook.com>
2024-04-04 11:30:51 -0400

    Implement xlsx generator
    Signed-off-by: Randy Bartels <rjbartels@outlook.com>

--- src/piidigger/filehandlers/__init__.py
+++ src/piidigger/filehandlers/__init__.py
@@ -1,4 +1,4 @@
 from piidigger.filehandlers import docx
 from piidigger.filehandlers import plaintext
 # from piidigger.filehandlers import xls
-#from piidigger.filehandlers import xlsx
+from piidigger.filehandlers import xlsx
